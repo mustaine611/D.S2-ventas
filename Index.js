c7eb56bc20f7103e4f289439711b05edae0e573b
paulo <paulo@metalmachine.dominiop>
2024-02-22 13:21:58 -0500

    añadiendo primer imagen catalogo

--- Index.js
+++ Index.js
@@ -1,1 +1,40 @@
+	const productos = [
+
+		{
+		id: 1,
+		nombre: "mesa",
+		precio: 80000
+		 
+
+		},
+		{
+		id: 2,
+		nombre:"enrrique",
+		precio: 20000
+		},
+		 	
+		{
+		id: 3,
+		nombre:"ramiro",
+		precio: 40000
+		}
+
+
+
+	];
+
+	let contadorCarrito = 0;
+	const carrito = [];
+
+	const ProductoCatalogoHTML = (producto) => "";
+	const ProductosCarritoHTML = (producto) => "";
+	
+	const MostrarCatalogo = () => {}:
+	const MostrarCarrito = () => {};
+
+	const BotonesCatalogo = () => {};
+	const BotonesCarrito = () => {};
+
+	MostrarCatalogo();
+
 
