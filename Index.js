4b1c1a3d086753b7a1c025450489a1b028122714
paulo <paulo@metalmachine.dominiop>
2024-02-22 21:20:19 -0500

    anexo imagenes productos

--- Index.js
+++ Index.js
@@ -1,4 +1,4 @@
-	const productos = [
+	const producto = [
 
 		{
 		id: 1,
@@ -26,7 +26,18 @@
 	let contadorCarrito = 0;
 	const carrito = [];
 
-	const ProductoCatalogoHTML = (producto) => "";
+	const ProductoCatalogoHTML = (producto) => {
+	const text = '<div class="col"> 
+			<div class="card" style="width: 18rem;">
+  		   		<img src="https://jsonplaceholder.typicode.com/${producto.id}" class="card-img-top" alt="...">
+  		   		<div class="card-body">
+    		      		<h5 class="card-title">Card title</h5>
+    		      			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    				<button href="#" class="btn btn-primary">Go somewhere</a>
+  		   		</div>
+			  </div>
+	   	      </div>';
+	};
 	const ProductosCarritoHTML = (producto) => "";
 	
 	const MostrarCatalogo = () => {}:
