d542602ead4cd0fade176f569788171b00c94dc3
Maria Camila Baena R <mariacamilabaenaramirez@gmail.com>
2024-02-24 20:57:25 -0500

    mod js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Función para cambiar el modo
 function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-  }
-  
-  // Escuchar el evento de clic en el botón y cambiar el modo
-  document.getElementById('toggle-mode').addEventListener('click', toggleDarkMode);
+  console.log('Cambiando modo');
+  document.body.classList.toggle('dark-mode');
+}
+
+// Escuchar el evento de clic en el botón y cambiar el modo
+document.getElementById('toggle-mode').addEventListener('click', toggleDarkMode);
