04cf2c0036a6a78c92e0a772eb9cae88d74894b2
Maria Camila Baena R <mariacamilabaenaramirez@gmail.com>
2024-02-25 22:16:04 -0500

    tiendajs

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function toggleDarkMode() {
 
 // Escuchar el evento de clic en el botón y cambiar el modo
 document.getElementById('toggle-mode').addEventListener('click', toggleDarkMode);
+
+document.getElementById('TiendaLink').addEventListener('click', function() {
+  // Obtén la referencia a la sección de la tienda y desplázate hacia ella
+  var TiendaSection = document.getElementById('TiendaSection');
+  TiendaSection.scrollIntoView({ behavior: 'smooth' });
+
+  // También puedes cambiar la URL en la barra de direcciones si estás utilizando enlaces reales
+  // window.location.href = '#tiendaSection';
+});
